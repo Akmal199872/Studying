7c9d8e5e984ab2b8d3177950354f1255ad0d5993
Akmal199872 <114590331+Akmal199872@users.noreply.github.com>
2022-10-18 21:30:04 +0300

    Сделал первое задание в третьей домашней работе

---
+++ Знакомство с языками программирования/Домашнее задание/homework_3/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Задача №19");
+System.Console.WriteLine("");
+
+int result = 0;
+
+while (true) {
+    System.Console.Write("Ввведите число 'палиндром': ");
+    int number_1 = int.Parse(Console.ReadLine());
+
+    string s_number_1 = number_1.ToString();
+    int size_1 = s_number_1.Length;
+    int count_1 = 1;
+
+    if (s_number_1[count_1 - 1] == s_number_1[size_1 - 1]) {
+        count_1++;
+        result = number_1;
+        break;
+    }
+    else {
+        System.Console.WriteLine("Число не является палиндромом. Введите число заново.");
+    }
+}
+System.Console.WriteLine($"Число, которое является палиндромом равно: {result}");
