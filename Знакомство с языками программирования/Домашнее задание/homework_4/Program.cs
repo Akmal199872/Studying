bbae4f2cf39f1b439c2424798d9bae40c8d3cb85
Akmal199872 <114590331+Akmal199872@users.noreply.github.com>
2022-10-21 02:23:08 +0300

    Прошёл семинар и сделал первое домашнее задание

---
+++ Знакомство с языками программирования/Домашнее задание/homework_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №25
+
+System.Console.WriteLine("Задача №25");
+System.Console.WriteLine("");
+
+System.Console.Write("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i < number_2; i++) {
+    for (int j = 0; j < number_2; j++) {
+        result = result * number_1;
+    }
+}
+
+System.Console.WriteLine($"Результатом возведения в степень {result}");
+
+
+
+
---
+++ Знакомство с языками программирования/Семинары/lesson_4/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача №1
+
+System.Console.WriteLine("Задача №1");
+
+int size_count(int number_1) {
+    string s_number_1 = number_1.ToString();
+    int size_1 = s_number_1.Length;
+    System.Console.WriteLine($"Количество цифр в числе {number_1} равно: {size_1}");
+    return size_1;
+}
+
+System.Console.WriteLine("Введите любое число:");
+int number_1 = int.Parse(Console.ReadLine());
+size_count(number_1);
+
+
+
+
+
+// Задача №2
+
+System.Console.WriteLine("");
+System.Console.WriteLine("Задача №2");
+System.Console.WriteLine("");
+
+int multiply_sum(int number_1) {
+    int result = 1;
+    for (int i = 1; i <= number_1; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+Random rnd = new Random();
+int number_2 = rnd.Next(1, 9);
+System.Console.WriteLine($"Созданное число равно: {number_2}");
+System.Console.WriteLine($"Факториалом числа {number_2} будет являться {multiply_sum(number_2)}");
+
+
+
+
+
+// Задача №3
+
+System.Console.WriteLine("");
+System.Console.WriteLine("Задача №3");
+System.Console.WriteLine("");
+
+int number_3 = rnd.Next(0, 1);
+int [] array_1 = new int[8];
+
+int fill_array(int number_1) {
+    for (int i = 0; i < array_1.Length; i++) {
+        array_1[i] = number_1;
+        System.Console.WriteLine($"Элемент массива: {array_1[i]}");
+    }
+    return number_1;
+}
+
+int print_array(int [] array) {
+    int result = 1;
+    for (int i = 0; i < array.Length; i++) {
+        result = array[i];
+        System.Console.WriteLine(result);
+    }
+    return result;
+}
+
+fill_array(number_3);
+print_array(array_1);
