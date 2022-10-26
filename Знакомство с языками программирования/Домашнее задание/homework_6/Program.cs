d700ca4596112650f375d1155c436cc16bb7eeee
Akmal199872 <114590331+Akmal199872@users.noreply.github.com>
2022-10-27 02:05:56 +0300

    Завершил домашнее задание

--- Знакомство с языками программирования/Домашнее задание/homework_6/Program.cs
+++ Знакомство с языками программирования/Домашнее задание/homework_6/Program.cs
@@ -6,6 +6,9 @@ using System.Diagnostics;
 
 System.Console.WriteLine("Задача №1");
 System.Console.WriteLine();
+System.Console.WriteLine("Краткое описание задачи. Вам требуется вводить исключительно числа каждые 5 секунд, иначе доступ к вводу прекратится.");
+System.Console.WriteLine("Количество чисел не ограничено. Ключевое в задаче: Покажет количество введённых чисел больше нуля.");
+System.Console.WriteLine();
 
 void PrintArray(int [] array) {
     string string_array = string.Join(" ", array);
@@ -59,7 +62,7 @@ void Timer() {
     }
 }
 
-// Timer();
+Timer();
 
 
 
@@ -68,6 +71,82 @@ void Timer() {
 System.Console.WriteLine();
 System.Console.WriteLine("Задача №2");
 System.Console.WriteLine();
+System.Console.WriteLine("Задача подразумевает вводить координаты отрезков. Программа позволит найти точки пересечения, указав координату.");
 
-// y = k1 * x + b1, y = k2 * x + b2
+int RandomNumbers() {
 
+    int [] array = new int [1];
+    int size = array.Length;
+
+    double y;
+    double x = 0.0;
+
+    for (int i = 1; i <= 4; i++) {
+        System.Console.Write($"Введите {i} значение для решения задачи: ");
+        string input_number = Console.ReadLine();
+        if (int.TryParse(input_number, out int number)) {
+            if (i < 3) {
+                System.Console.WriteLine($"A{i} = {number}");
+                Array.Resize(ref array, size++);
+                array[size-2] = number;
+                PrintArray(array);
+            }
+            else {
+                System.Console.WriteLine($"B{i-2} = {number}");
+                Array.Resize(ref array, size++);
+                array[size-2] = number;
+                PrintArray(array);          
+            }
+        }
+        else {
+            System.Console.WriteLine("Введено не число. Попробуйте ещё раз.");
+            i--;
+            System.Console.WriteLine();
+        }
+    }
+
+    System.Console.WriteLine();
+    PrintArray(array);
+    System.Console.WriteLine($"Сформировано следующее уравнение: y = {array[0]}x + {array[2]}, y = {array[1]}x + {array[3]}");
+    
+    // while (true) {
+    //     string string_statement = ($"y = {array[0]} * {x} + {array[2]}, y = {array[1]} * {x} + {array[3]}");
+    //     System.Console.WriteLine(string_statement);
+
+    //     double result_x_1 = array[0] * x + array[2];
+    //     double result_x_2 = array[1] * x + array[3];
+    //     y = result_x_1 - result_x_2;
+
+    //     if (y == 0) {
+    //         System.Console.WriteLine($"Неизвестная x = {x}");
+    //         y = result_x_2;
+    //         System.Console.WriteLine($"Неизвестная y = {y}");    
+    //         System.Console.WriteLine($"Точка пересечения: ({x}, {y})");
+    //         break;
+    //     }
+    //     else {
+    //         x -= 0.1;
+    //     }  
+    // }
+
+    double [] array_2 = array.Select(z => Convert.ToDouble(z)).ToArray();
+
+    if (array_2[0] == array_2[2] && array_2[1] == array_2[3]) {
+        System.Console.WriteLine("Прямые отрезки совпадают.");
+    }
+    else if (array_2[0] == array_2[2]) {
+        System.Console.WriteLine("Прямые отрезки расположены параллельно.");
+    }
+    else {
+        x = -(array_2[2] - array_2[3]) / (array_2[0] - array_2[1]);
+        string string_statement = ($"x = - ({array_2[2]} - {array_2[3]}) / ({array_2[0]} - {array_2[1]})");
+        System.Console.WriteLine(string_statement);
+        y = array_2[0] * (-(array_2[2] - array_2[3]) / (array_2[0] - array_2[1])) + array_2[2];
+
+        System.Console.WriteLine($"Неизвестная x = {x}");
+        System.Console.WriteLine($"Неизвестная y = {y}");
+    }
+    return 0;
+}
+
+RandomNumbers();
